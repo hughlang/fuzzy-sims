3740dcb27bb96c7d48e67f7bce45febe4189022c
Hugh Lang <hughlang@gmail.com>
2019-08-05 15:05:50 -0400

    Slots enum for Play

--- src/game/slots.rs
+++ src/game/slots.rs
@@ -6,13 +6,27 @@ extern crate js_sys;
 use wasm_bindgen::prelude::*;
 use rand::prelude::*;
 use itertools::join;
+use std::collections::HashSet;
 
 const SLOT_COUNT: usize = 6;
+
+/// A Play is a grouping of nums that translate to a score. When numbers are generated
+/// for Slots,
+#[derive(Clone, Eq, PartialEq, Hash)]
+pub enum Play {
+    /// The highest number not used in other Ranks
+    HighNum(u32),
+    /// A set of matching numbers (e.g. pair, 3 of a kind)
+    NumMatch(usize, u32),
+    /// A sequence of numbers
+    NumSequence(Vec<u32>),
+}
 
 #[wasm_bindgen]
 #[derive(Clone)]
 pub struct Slots {
     nums: Vec<u32>,
+    plays: HashSet<Play>,
 }
 
 #[wasm_bindgen]
@@ -20,7 +34,8 @@ impl Slots {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
         Slots {
-            nums: Vec::with_capacity(SLOT_COUNT)
+            nums: Vec::with_capacity(SLOT_COUNT),
+            plays: HashSet::new(),
         }
     }
 
@@ -33,6 +48,8 @@ impl Slots {
         }
     }
 
+    /// Copied from: https://github.com/stevenpack/bob-ross-lipsum-rust/blob/master/src/phrases.rs
+    /// See also: https://blog.cloudflare.com/cloudflare-workers-as-a-serverless-rust-platform/
     fn get_rng(&self) -> SmallRng {
         use js_sys::Date;
         use rand::SeedableRng;
@@ -48,33 +65,44 @@ impl Slots {
         let values = join(&self.nums, "|");
         values
     }
+
+    /// Apply common slot machine rules for winning:
+    /// * N of a kind
+    /// * High score
+    ///
+    ///
+    pub fn evaluate_nums(&mut self) {
+        let nums = self.nums.clone();
+
+
+    }
 }
 
 fn transmute(x: u128) -> [u8; 16] {
 
-        let b1 : u8 = ((x >> 120) & 0xffffffff) as u8;
-        let b2 : u8 = ((x >> 112) & 0xffffffff) as u8;
-        let b3 : u8 = ((x >> 104) & 0xffffffff) as u8;
-        let b4 : u8 = ((x >> 96) & 0xffffffff) as u8;
+    let b1 : u8 = ((x >> 120) & 0xffffffff) as u8;
+    let b2 : u8 = ((x >> 112) & 0xffffffff) as u8;
+    let b3 : u8 = ((x >> 104) & 0xffffffff) as u8;
+    let b4 : u8 = ((x >> 96) & 0xffffffff) as u8;
 
-        let b5 : u8 = ((x >> 88) & 0xffffffff) as u8;
-        let b6 : u8 = ((x >> 80) & 0xffffffff) as u8;
-        let b7 : u8 = ((x >> 72) & 0xffffffff) as u8;
-        let b8 : u8 = ((x >> 64) & 0xffffffff) as u8;
+    let b5 : u8 = ((x >> 88) & 0xffffffff) as u8;
+    let b6 : u8 = ((x >> 80) & 0xffffffff) as u8;
+    let b7 : u8 = ((x >> 72) & 0xffffffff) as u8;
+    let b8 : u8 = ((x >> 64) & 0xffffffff) as u8;
 
-        let b9 : u8 = ((x >> 56) & 0xffffffff) as u8;
-        let b10 : u8 = ((x >> 48) & 0xffffffff) as u8;
-        let b11 : u8 = ((x >> 40) & 0xffffffff) as u8;
-        let b12 : u8 = ((x >> 32) & 0xffffffff) as u8;
+    let b9 : u8 = ((x >> 56) & 0xffffffff) as u8;
+    let b10 : u8 = ((x >> 48) & 0xffffffff) as u8;
+    let b11 : u8 = ((x >> 40) & 0xffffffff) as u8;
+    let b12 : u8 = ((x >> 32) & 0xffffffff) as u8;
 
-        let b13 : u8 = ((x >> 24) & 0xffffffff) as u8;
-        let b14 : u8 = ((x >> 16) & 0xffffffff) as u8;
-        let b15 : u8 = ((x >> 8) & 0xffffffff) as u8;
-        let b16 : u8 = (x & 0xffffffff) as u8;
+    let b13 : u8 = ((x >> 24) & 0xffffffff) as u8;
+    let b14 : u8 = ((x >> 16) & 0xffffffff) as u8;
+    let b15 : u8 = ((x >> 8) & 0xffffffff) as u8;
+    let b16 : u8 = (x & 0xffffffff) as u8;
 
-        //Most of the entropy is in the last few bytes and generators are allowed
-        //to assume evenly spread entropy in the seed, so spread the bytes around
-        [b16, b1, b14, b3, b12, b5, b10, b7, b8, b9, b6, b11, b4, b13, b2, b15]
+    //Most of the entropy is in the last few bytes and generators are allowed
+    //to assume evenly spread entropy in the seed, so spread the bytes around
+    [b16, b1, b14, b3, b12, b5, b10, b7, b8, b9, b6, b11, b4, b13, b2, b15]
 }
 
 #[cfg(test)]
